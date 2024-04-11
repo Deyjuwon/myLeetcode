109f40794eb9743749a766eaf30f4ae2f00dbb51
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-04-11 23:34:21 +0100

    longest common prefix

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -3,18 +3,18 @@ def longestCommonPrefix( strs):
         :type strs: List[str]
         :rtype: str
         """
-        arr = []
-        for i in strs:
-                arr.append(i[0:2])
-        print(arr)
-        for x in range(len(arr)):
-                for y in range(len(arr)+1):
-                        if x == y:
-                                print(arr[x])
-                        
+        if not strs:
+            return ''
+
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return strs[0][:i]
+
+        return strs[0]
                 
 
 
 
-longestCommonPrefix(["ower","flow","flight"])
+
         
